165f812a2a58bca636b2fd0660b5954995f0d4f5
Eric Pardee <eric@atlasdigital.tv>
2013-02-06 18:26:07 -0800

    [COOK-2370] added escaping

--- providers/app.rb
+++ providers/app.rb
@@ -26,8 +26,8 @@ include Windows::Helper
 action :add do
 	unless @current_resource.exists
     cmd = "#{appcmd} add app /site.name:\"#{@new_resource.app_name}\""
-    cmd << " /path:#{@new_resource.path}"
-    cmd << " /applicationPool:#{@new_resource.application_pool}" if @new_resource.application_pool
+    cmd << " /path:\"#{@new_resource.path}\""
+    cmd << " /applicationPool:\"#{@new_resource.application_pool}\"" if @new_resource.application_pool
     cmd << " /physicalPath:\"#{@new_resource.physical_path}\"" if @new_resource.physical_path
     Chef::Log.debug(cmd)
     shell_out!(cmd)
@@ -40,8 +40,8 @@ end
 action :config do
 
   cmd = "#{appcmd} set app \"#{site_identifier}\""
-	cmd << " /path:#{@new_resource.path}" if @new_resource.path
-	cmd << " /applicationPool:#{@new_resource.application_pool}" if @new_resource.application_pool
+	cmd << " /path:\"#{@new_resource.path}\"" if @new_resource.path
+	cmd << " /applicationPool:\"#{@new_resource.application_pool}\"" if @new_resource.application_pool
   Chef::Log.debug(cmd)
   shell_out!(cmd)
 
