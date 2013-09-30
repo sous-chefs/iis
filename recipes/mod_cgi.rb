da0a6f4360cca1f2f7b616103c5c7776dfe6839c
Richard Downer <richard.downer@cloudsoftcorp.com>
2013-09-30 15:47:42 -0400

    [COOK-3675] Add a recipe for CGI feature
    Signed-off-by: Seth Vargo <sethvargo@gmail.com>

---
+++ recipes/mod_cgi.rb
@@ -0,0 +1,26 @@
+#
+# Author:: Richard Downer (<richard.downer@cloudsoftcorp.com>)
+# Cookbook Name:: iis
+# Recipe:: mod_cgi
+#
+# Copyright 2013, Cloudsoft Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "iis"
+
+webpi_product "CGI" do
+  accept_eula node['iis']['accept_eula']
+  action :install
+end
