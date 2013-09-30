fc9c42d99063aa520d9f7808b496945c02c3a715
Matt Clifton <spartacus003@hotmail.com>
2013-09-30 15:23:31 -0700

    Added EnabledProtocols option for App

--- providers/app.rb
+++ providers/app.rb
@@ -30,6 +30,7 @@ action :add do
     cmd << " /path:\"#{@new_resource.path}\""
     cmd << " /applicationPool:\"#{@new_resource.application_pool}\"" if @new_resource.application_pool
     cmd << " /physicalPath:\"#{@new_resource.physical_path}\"" if @new_resource.physical_path
+	cmd << " /enabledProtocols:\"#{@new_resource.enabled_protocols}\"" if @new_resource.enabled_protocols
     Chef::Log.debug(cmd)
     shell_out!(cmd)
     Chef::Log.info("App created")
@@ -43,6 +44,7 @@ action :config do
   cmd = "#{appcmd} set app \"#{site_identifier}\""
   cmd << " /path:\"#{@new_resource.path}\"" if @new_resource.path
   cmd << " /applicationPool:\"#{@new_resource.application_pool}\"" if @new_resource.application_pool
+  cmd << " /enabledProtocols:\"#{@new_resource.enabled_protocols}\"" if @new_resource.enabled_protocols
   Chef::Log.debug(cmd)
   shell_out!(cmd)
 
--- resources/app.rb
+++ resources/app.rb
@@ -24,6 +24,7 @@ attribute :app_name, :kind_of => String, :name_attribute => true
 attribute :path, :kind_of => String, :default => '/'
 attribute :application_pool, :kind_of => String
 attribute :physical_path, :kind_of => String
+attribute :enabled_protocols, :kind_of => String
 attr_accessor :exists, :running
 
 def initialize(*args)
