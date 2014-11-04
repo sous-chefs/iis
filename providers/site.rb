84dd9fe31607f245b9bd15a05c645e7ef250d591
Gary Hall <gary@4mm.co.uk>
2014-11-04 20:23:57 +0000

    Catered for edge case where user leaves trailing forwardslash or backslash in path and appcmd borks.

--- providers/site.rb
+++ providers/site.rb
@@ -27,7 +27,7 @@ action :add do
   unless @current_resource.exists
     cmd = "#{appcmd} add site /name:\"#{@new_resource.site_name}\""
     cmd << " /id:#{@new_resource.site_id}" if @new_resource.site_id
-    cmd << " /physicalPath:\"#{win_friendly_path(@new_resource.path)}\"" if @new_resource.path
+    cmd << " /physicalPath:\"#{win_friendly_path(sanitize_path(@new_resource.path))}\"" if @new_resource.path
   if @new_resource.bindings
     cmd << " /bindings:#{@new_resource.bindings}"
   else
@@ -163,4 +163,9 @@ end
 
 def site_identifier
   @new_resource.host_header || @new_resource.site_name
+end
+
+def sanitize_path(path)
+  path.chomp!('/')
+  path.chomp!('\\')
 end
