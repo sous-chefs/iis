f4f0cbc044d963d25668ef7e028a6f6b5268c825
Justin Schuhmann <justin.schuhmann@e-hps.com>
2014-12-30 16:33:40 -0500

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures Microsoft Internet Information Services"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "3.0.1"
+version          "3.0.2"
 supports         "windows"
 depends          "windows", ">= 1.34.6"
 depends          "chef-client", ">= 3.7.0"
