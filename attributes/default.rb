3940bd4aa7dcd0dafda6e7b1e2c7a1cc488f08af
Justin Schuhmann <justin.schuhmann@e-hps.com>
2015-02-03 10:50:48 -0500

    fix issue with inheritance in attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,11 +19,11 @@
 #
 
 default['iis']['home']       = "#{ENV['WINDIR']}\\System32\\inetsrv"
-default['iis']['conf_dir']   = "#{iis['home']}\\config"
+default['iis']['conf_dir']   = "#{ENV['WINDIR']}\\System32\\inetsrv\\config"
 default['iis']['pubroot']    = "#{ENV['SYSTEMDRIVE']}\\inetpub"
-default['iis']['docroot']    = "#{iis['pubroot']}\\wwwroot"
-default['iis']['log_dir']    = "#{iis['pubroot']}\\logs\\LogFiles"
-default['iis']['cache_dir']  = "#{iis['pubroot']}\\temp"
+default['iis']['docroot']    = "#{ENV['SYSTEMDRIVE']}\\inetpub\\wwwroot"
+default['iis']['log_dir']    = "#{ENV['SYSTEMDRIVE']}\\inetpub\\logs\\LogFiles"
+default['iis']['cache_dir']  = "#{ENV['SYSTEMDRIVE']}\\inetpub\\temp"
 
 if Opscode::IIS::Helper.older_than_windows2008r2?
   default['iis']['components'] = %w{Web-Server}
