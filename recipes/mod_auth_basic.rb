4f1c9fe074a80e8c62b605df868fcd97e7cea85a
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2015-02-12 17:36:46 -0500

    OpsCode, Inc -> Chef Software, Inc

--- recipes/mod_auth_basic.rb
+++ recipes/mod_auth_basic.rb
@@ -3,7 +3,7 @@
 # Cookbook Name:: iis
 # Recipe:: mod_auth_basic
 #
-# Copyright:: Copyright (c) 2011 Opscode, Inc.
+# Copyright:: Copyright (c) 2011 Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
