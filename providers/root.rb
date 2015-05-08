d8f372885a7ec5dbc52d2fdd9cdb7ebbb4e41400
Justin Schuhmann <jmschu02@gmail.com>
2015-05-07 20:12:19 -0400

    added mime_maps
    iis_root now also has mime_maps
    
    ```ruby
    iis_root 'this is still a placeholder for now' do
      mime_maps ["fileExtension='syx',mimeType='application/octet-stream'","fileExtension='txt',mimeType='application/octet-stream'"]
    end
    ```
    
    The above will change the mimeMaps, be aware that I recommend using the default and then adding to it since there are like 100 mimeMaps by default.

--- providers/root.rb
+++ providers/root.rb
@@ -28,41 +28,13 @@ include Opscode::IIS::Helper
 action :config do
 	was_updated = false
 
-	# handles default documents
-  cmd = shell_out "#{appcmd(node)} list config /section:defaultDocument /config:* /xml"
-  if cmd.stderr.empty?
-    xml = cmd.stdout
-    doc = Document.new xml
-		current_default_documents = XPath.match(doc.root, '//files/add/@value').map{|x| x.value}
+	was_updated = default_documents || was_updated
+	was_updated = mime_maps || was_updated
 
-		cmd = "#{appcmd(node)} set config /section:defaultDocument"
-		Chef::Log.info("#{current_default_documents}: #{new_resource.default_documents}")
-
-		new_resource.default_documents.each do |document|
-			Chef::Log.info("#{document}: #{!current_default_documents.include? document}")
-			if !current_default_documents.include? document
-				cmd << " /+files.[value='#{document}']"
-			end
-		end
-
-		current_default_documents.each do |document|
-			Chef::Log.info("#{document}: #{!new_resource.default_documents.include? document}")
-			if !new_resource.default_documents.include? document
-				cmd << " /-files.[value='#{document}']"
-			end
-		end
-
-		if cmd != "#{appcmd(node)} set config /section:defaultDocument"
-			was_updated = true
-			shell_out! cmd
-		end
-
-		if was_updated
-	    new_resource.updated_by_last_action(true)
-		  Chef::Log.info('Default Documents updated')
-	  else
-	    Chef::Log.debug("#{new_resource} Default Documents exist - nothing to do")
-		end
+	if was_updated
+    new_resource.updated_by_last_action(true)
+  else
+    Chef::Log.debug("#{new_resource} - nothing to do")
 	end
 end
 
@@ -70,4 +42,66 @@ def load_current_resource
   @current_resource = Chef::Resource::IisRoot.new(new_resource.name)
   @current_resource.default_documents(new_resource.default_documents)
   @current_resource.default_documents_enabled(new_resource.default_documents_enabled)
+  @current_resource.mime_maps(new_resource.mime_maps)
 end
+
+private
+	def default_documents
+		# handles default documents
+	  cmd = shell_out "#{appcmd(node)} list config /section:defaultDocument /config:* /xml"
+	  if cmd.stderr.empty?
+	    xml = cmd.stdout
+	    doc = Document.new xml
+			current_default_documents = XPath.match(doc.root, '//system.webServer-defaultDocument/files/add/@value').map{|x| x.value}
+
+			cmd = "#{appcmd(node)} set config /section:defaultDocument"
+
+			new_resource.default_documents.each do |document|
+				if !current_default_documents.include? document
+					cmd << " /+files.[value='#{document}']"
+				end
+			end
+
+			current_default_documents.each do |document|
+				if !new_resource.default_documents.include? document
+					cmd << " /-files.[value='#{document}']"
+				end
+			end
+
+			if cmd != "#{appcmd(node)} set config /section:defaultDocument"
+				shell_out! cmd
+				Chef::Log.info('Default Documents updated')
+				was_updated = true
+			end
+		end
+	end
+
+	def mime_maps
+		# handles mime maps
+	  cmd = shell_out "#{appcmd(node)} list config /section:staticContent /config:* /xml"
+	  if cmd.stderr.empty?
+	    xml = cmd.stdout
+	    doc = Document.new xml
+			current_mime_maps = XPath.match(doc.root, '//system.webServer-staticContent/mimeMap').map{|x| "fileExtension='#{x.attribute 'fileExtension'}',mimeType='#{x.attribute 'mimeType'}'" }
+
+			cmd = "#{appcmd(node)} set config /section:staticContent"
+
+			new_resource.mime_maps.each do |mime_map|
+				if !current_mime_maps.include? mime_map
+					cmd << " /+\"[#{mime_map}]\""
+				end
+			end
+
+			current_mime_maps.each do |mime_map|
+				if !new_resource.mime_maps.include? mime_map
+					cmd << " /+\"[#{mime_map}]\""
+				end
+			end
+
+			if cmd != "#{appcmd(node)} set config /section:staticContent"
+				shell_out! cmd
+				Chef::Log.info('mime maps updated')
+				was_updated = true
+			end
+		end
+	end
