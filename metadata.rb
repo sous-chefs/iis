e58a4781c2f2beede3f8590daba65d65dadd0709
Tim Smith <tsmith@chef.io>
2016-07-19 13:46:09 -0700

    Add chef_version metadata Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -9,3 +9,5 @@ supports 'windows'
 depends 'windows', '>= 1.34.6'
 source_url 'https://github.com/chef-cookbooks/iis' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/iis/issues' if respond_to?(:issues_url)
+
+chef_version '>= 11.0' if respond_to?(:chef_version)
