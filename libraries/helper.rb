b4b503d0721c3f367410901acd5f28453dadeee9
Tim Smith <tsmith@chef.io>
2016-09-06 15:35:28 -0700

    Update copyrights
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -5,7 +5,7 @@
 # Author:: Julian C. Dunn <jdunn@chef.io>
 # Author:: Justin Schuhmann <jmschu02@gmail.com>
 #
-# Copyright 2013, Chef Software, Inc.
+# Copyright 2013-2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,7 +1,7 @@
 #
 # Author:: Kartik Cating-Subramanian (<ksubramanian@chef.io>)
 #
-# Copyright 2015, Chef Software, Inc.
+# Copyright 2015-2016, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
