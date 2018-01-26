d1389df866ab7b6c3cf5a50fee67e85565f6abc4
Richard Lavey <richard.lavey@calastone.com>
2018-01-26 16:30:38 +0000

    fix rubocop violations
    Signed-off-by: Richard Lavey <richard.lavey@calastone.com>

--- resources/config_property.rb
+++ resources/config_property.rb
@@ -25,7 +25,8 @@ property :filter, String, required: true
 property :value, [String, Integer], required: true
 
 action :set do
-  location_param = "-location \"#{new_resource.location}\"" if property_is_set?(:location)
+  location_param = "-location \"#{new_resource.location}\"" if
+    property_is_set?(:location)
 
   # powershell doesn't like { or } in xpath values (e.g. server variables)
   escaped_filter = new_resource.filter.gsub('{', '{{').gsub('}', '}}')
@@ -35,46 +36,66 @@ action :set do
                     else
                       "\"#{new_resource.value}\""
                     end
-  powershell_script "Set #{new_resource.ps_path}#{new_resource.location}/#{escaped_filter}/#{new_resource.property}" do
+  powershell_script "Set #{new_resource.ps_path}#{new_resource.location}\
+    /#{escaped_filter}/#{new_resource.property}" do
     code <<-EOH
-    Set-WebConfigurationProperty -pspath "#{new_resource.ps_path}" #{location_param} -filter "#{escaped_filter}" -name "#{new_resource.property}" -value #{property_value} -ErrorAction Stop
+    Set-WebConfigurationProperty -pspath "#{new_resource.ps_path}" \
+    #{location_param} -filter "#{escaped_filter}" \
+    -name "#{new_resource.property}" \
+    -value #{property_value} -ErrorAction Stop
     EOH
     only_if <<-EOH
-    (Get-WebConfigurationProperty -pspath "#{new_resource.ps_path}" #{location_param} -filter "#{escaped_filter}" -name "#{new_resource.property}" -ErrorAction Stop) -ne #{property_value}
+    (Get-WebConfigurationProperty -pspath "#{new_resource.ps_path}" \
+    #{location_param} -filter "#{escaped_filter}" \
+    -name "#{new_resource.property}" -ErrorAction Stop) -ne #{property_value}
     EOH
   end
 end
 
 action :add do
-  location_param = "-location \"#{new_resource.location}\"" if property_is_set?(:location)
+  location_param = "-location \"#{new_resource.location}\"" if
+    property_is_set?(:location)
 
   # powershell doesn't like { or } in xpath values (e.g. server variables)
   escaped_value = new_resource.value.gsub('{', '{{').gsub('}', '}}')
   escaped_filter = new_resource.filter.gsub('{', '{{').gsub('}', '}}')
 
-  powershell_script "Set #{new_resource.ps_path}#{new_resource.location}/#{escaped_filter}/#{new_resource.property}" do
+  powershell_script "Set #{new_resource.ps_path}#{new_resource.location}\
+    /#{escaped_filter}/#{new_resource.property}" do
     code <<-EOH
-    Add-WebConfigurationProperty -pspath "#{new_resource.ps_path}" #{location_param} -filter "#{escaped_filter}" -name "." -value @{ #{new_resource.property} = '#{new_resource.value}'; } -ErrorAction Stop
+    Add-WebConfigurationProperty -pspath "#{new_resource.ps_path}" \
+    #{location_param} -filter "#{escaped_filter}" \
+    -name "." -value @{ #{new_resource.property} = '#{new_resource.value}'; } \
+    -ErrorAction Stop
     EOH
     only_if <<-EOH
-    (Get-WebConfiguration -pspath "#{new_resource.ps_path}" #{location_param} -filter "#{escaped_filter}/*[@#{new_resource.property}='#{escaped_value}']" -ErrorAction Stop) -eq $null
+    (Get-WebConfiguration -pspath "#{new_resource.ps_path}" #{location_param} \
+    -filter "#{escaped_filter}/*[@#{new_resource.property}='#{escaped_value}']" \
+    -ErrorAction Stop) -eq $null
     EOH
   end
 end
 
 action :remove do
-  location_param = "-location \"#{new_resource.location}\"" if property_is_set?(:location)
+  location_param = "-location \"#{new_resource.location}\"" if
+    property_is_set?(:location)
 
   # powershell doesn't like { or } in xpath values (e.g. server variables)
   escaped_value = new_resource.value.gsub('{', '{{').gsub('}', '}}')
   escaped_filter = filter.gsub('{', '{{').gsub('}', '}}')
 
-  powershell_script "Set #{new_resource.ps_path}#{new_resource.location}/#{escaped_filter}/#{new_resource.property}" do
+  powershell_script "Set #{new_resource.ps_path}#{new_resource.location}\
+    /#{escaped_filter}/#{new_resource.property}" do
     code <<-EOH
-    Remove-WebConfigurationProperty -pspath "#{new_resource.ps_path}" #{location_param} -filter "#{escaped_filter}" -name "." -AtElement @{ #{new_resource.property} = '#{new_resource.value}'; } -ErrorAction Stop
+    Remove-WebConfigurationProperty -pspath "#{new_resource.ps_path}" \
+    #{location_param} -filter "#{escaped_filter}" \
+    -name "." -AtElement @{ #{new_resource.property} = \
+    '#{new_resource.value}'; } -ErrorAction Stop
     EOH
     only_if <<-EOH
-    (Get-WebConfiguration -pspath "#{new_resource.ps_path}" #{location_param} -filter "#{escaped_filter}/*[@#{new_resource.property}='#{escaped_value}']" -ErrorAction Stop) -ne $null
+    (Get-WebConfiguration -pspath "#{new_resource.ps_path}" #{location_param} \
+    -filter "#{escaped_filter}/*[@#{new_resource.property}='#{escaped_value}']" \
+    -ErrorAction Stop) -ne $null
     EOH
   end
 end
