c83cc6cd20ba69ec9ec4acb1a5cdbabbb94e72cf
Jeffrey Coe <jeffrey.coe@cerner.com>
2018-10-11 13:24:41 -0400

    Update iis_config_property add action to support multiple properties
    Signed-off-by: Jeffrey Coe <jeffrey.coe@cerner.com>

--- resources/config_property.rb
+++ resources/config_property.rb
@@ -18,13 +18,17 @@
 #
 # Configures an IIS property (using powershell for idempotence)
 
+include IISCookbook::Helper
+
 property :property, String, name_property: true
 property :ps_path, String, required: true
 property :location, String
 property :filter, String, required: true
-property :value, [String, Integer], required: true
+property :value, [String, Integer, Hash], required: true
 
 action :set do
+  raise 'Set action does not support a Hash type for the value property' if new_resource.value.is_a?(Hash)
+
   location_param = "-location \"#{new_resource.location}\"" if
     property_is_set?(:location)
 
@@ -36,6 +40,7 @@ action :set do
                     else
                       "\"#{new_resource.value}\""
                     end
+
   powershell_script "Set #{new_resource.ps_path}#{new_resource.location}\
 /#{escaped_filter}/#{new_resource.property}" do
     code <<-EOH
@@ -56,27 +61,43 @@ action :add do
   location_param = "-location \"#{new_resource.location}\"" if
     property_is_set?(:location)
 
-  # powershell doesn't like { or } in xpath values (e.g. server variables)
-  escaped_value = new_resource.value.gsub('{', '{{').gsub('}', '}}')
   escaped_filter = new_resource.filter.gsub('{', '{{').gsub('}', '}}')
+  add_cmd = "Add-WebConfigurationProperty -ErrorAction Stop -PSPath #{new_resource.ps_path} #{location_param}"\
+    "-Filter \"#{escaped_filter}\" -Name \".\""
+
+  # powershell doesn't like { or } in xpath values (e.g. server variables)
+  if new_resource.value.is_a?(Hash)
+    add_cmd << " -Value #{to_powershell_hash(new_resource.value)}"
+    guard_xpath = ''
+
+    new_resource.value.each_with_index do |(k, v), idx|
+      escaped_value = v.gsub('{', '{{').gsub('}', '}}')
+      guard_xpath << "@#{k}=\'#{escaped_value}\'"
+      guard_xpath << ' and ' unless idx == new_resource.value.size - 1
+    end
+
+    guard_xpath = "*[#{guard_xpath}]"
+  else
+    escaped_value = new_resource.value.gsub('{', '{{').gsub('}', '}}')
+    add_cmd = "#{add_cmd} -Value @{ #{new_resource.property} = '#{new_resource.value}'; }"
+    guard_xpath = "*[@#{new_resource.property}='#{escaped_value}']"
+  end
+
+  guard_cmd = <<-EOH
+  (Get-WebConfiguration -PSPath "#{new_resource.ps_path}" #{location_param} \
+  -Filter \"#{escaped_filter}/#{guard_xpath}\" -ErrorAction Stop) -eq $null
+  EOH
 
   powershell_script "Set #{new_resource.ps_path}#{new_resource.location}\
 /#{escaped_filter}/#{new_resource.property}" do
-    code <<-EOH
-    Add-WebConfigurationProperty -pspath "#{new_resource.ps_path}" \
-    #{location_param} -filter "#{escaped_filter}" \
-    -name "." -value @{ #{new_resource.property} = '#{new_resource.value}'; } \
-    -ErrorAction Stop
-    EOH
-    only_if <<-EOH
-    (Get-WebConfiguration -pspath "#{new_resource.ps_path}" #{location_param} \
-    -filter "#{escaped_filter}/*[@#{new_resource.property}='#{escaped_value}']" \
-    -ErrorAction Stop) -eq $null
-    EOH
+    code add_cmd
+    only_if guard_cmd
   end
 end
 
 action :remove do
+  raise 'Remove action does not support a Hash type for the value property' if new_resource.value.is_a?(Hash)
+
   location_param = "-location \"#{new_resource.location}\"" if
     property_is_set?(:location)
 
