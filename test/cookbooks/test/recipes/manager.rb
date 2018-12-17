a055c149b2288b45f2f2b2e81a297af25cce7c73
Jason Field <jason.field@calastone.com>
2018-12-17 14:25:20 +0000

    Style/StringLiterals fix
    Signed-off-by: Jason Field <jason@avon-lea.co.uk>

--- test/cookbooks/test/recipes/manager.rb
+++ test/cookbooks/test/recipes/manager.rb
@@ -3,5 +3,5 @@ include_recipe 'iis'
 iis_manager 'IIS Manager' do
   port                      19500
   enable_remote_management  true
-  log_directory             "C:\\CustomPath"
+  log_directory             'C:\\CustomPath'
 end
