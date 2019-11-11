876f417995259a9294a8995995e794c03f2604e3
Tim Smith <tsmith84@gmail.com>
2019-11-10 17:54:09 -0800

    Remove deprecated metadata
    No need for either of these anymore
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,10 +3,9 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Installs/Configures Microsoft Internet Information Services'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '7.2.0'
 supports 'windows'
 depends 'windows', '>= 4.1.0'
 source_url 'https://github.com/chef-cookbooks/iis'
 issues_url 'https://github.com/chef-cookbooks/iis/issues'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+chef_version '>= 12.14'
